293ab533a0186e774eb0eab835adb362a3386545
mmoghul <mmoghul@live.unc.edu>
2021-09-26 18:35:17 -0400

    ex04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -3,10 +3,8 @@
 __author__ = "123456789"
 
 # ________ALL__________
-
-
 def all(nums: list, num: int) -> bool:
- """return true if all numbers are equal, otherwise return false"""
+    """return true if all numbers are equal, otherwise return false"""
     if len(nums) == 0:
         return False
     size = len(nums) - 1
@@ -15,14 +13,10 @@ def all(nums: list, num: int) -> bool:
             return False
         size -= 1
     return True
-   
-
 
 # _______IS EQUAL_______
-
-
 def is_equal(n1: list[int], n2: list[int]) -> bool:
-"""return True if every element at every index is equal in both lists, else return false"""
+    """return True if every element at every index is equal in both lists, else return false"""
     if len(n1) != len(n2):
         return False
     size = len(n1) - 1
@@ -32,25 +26,23 @@ def is_equal(n1: list[int], n2: list[int]) -> bool:
         size -= 1
     return True
 
-
 # _______MAX_______
-
-
-def max(input: list[int]) -> int:
-"""return max vlaue, or if list is empty, return error message"""
-    if len(input) == 0:
+def max(inp: list[int]) -> int:
+    """return max vlaue, or if list is empty, return error message"""
+    if len(inp) == 0:
         raise ValueError("max() arg is an empty List")
-    maximum = input[0]
-    i = 1
-    while i < len(input) - 1:
-        if input[i] > maximum:
-            maximum = input[i]
+    maximum = inp[0]
+    i = 0
+
+    while i < len(inp) - 1: 
+        if inp[i] > maximum:
+            maximum = inp[i]
         i += 1
-    return input[i]
+    return maximum
 
 
 def main():
-    max([56, 863, 33])
+    print(max([56, 47, 33]))
     is_equal([543, 907, 123], [534543, 98, 1])
 
 
