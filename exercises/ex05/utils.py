35b2439096ee2f281466834fe82448d717e0030a
mmoghul <mmoghul@live.unc.edu>
2021-10-01 22:25:08 -0400

    ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -2,4 +2,32 @@
 
 __author__ = "123456789"
 
-# Define your functions below
+
+def only_evens(ints: list[int]) -> list[int]:
+    result: list[int] = []
+    for i in ints:
+        if i % 2 == 0:
+            result.append(i)
+    return result
+
+
+def sub(l: list[int], start: int, end: int) -> list[int]:
+    if start < 0:
+        start = 0
+    if end > len(l):
+        end = len(l)
+    if len(l) == 0 or start > len(l) or end <= 0:
+        return []
+    return l[start:end]
+
+
+def concat(list1: list[int], list2: list[int]) -> list[int]:
+    list3 = []
+    for i in list1:
+        list3.append(i)
+    for i in list2:
+        list3.append(i)
+    return list3
+
+    # list3 = list1 + list2
+    # return list3
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,70 @@
 """Unit tests for list utility functions."""
-
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
 __author__ = "123456789"
+
+#  evens use case 1
+def test_evens1() -> None:
+    assert only_evens([2, 4, 3, 6]) == [2, 4, 6]
+
+
+# evens use case 2
+def test_evens2() -> None:
+    assert only_evens([5, 8, 44, 78]) == [8, 44, 78]
+
+
+# evens edge case
+def test_evens3() -> None:
+    assert only_evens([1, 3, 7, 9, 12]) == [12]
+
+
+# sub use case 1
+def test_sub1() -> None:
+    assert sub([2, 4, 6, 2, 1], 2, 4) == [6, 2]
+
+
+# sub use case 2
+def test_sub2() -> None:
+    assert sub([6, 89, 75, 3, 4, 34, 567, 132], 7, 1) == []
+
+
+# edge case
+def test_sub3() -> None:
+    assert sub([2, 4, 6, 5, 2, 3], -1, 20) == [2, 4, 6, 5, 2, 3]
+
+
+# use case 1
+def test_concat1() -> None:
+    assert concat([345, 6798, 2], [243, 786, 3, 35, 11]) == [
+        345,
+        6798,
+        2,
+        243,
+        786,
+        3,
+        35,
+        11,
+    ]
+
+
+# use case 2
+def test_concat2() -> None:
+    assert concat([465, 78, 243, 576, 789, 5], [7, 23, 58, 79, 25432]) == [
+        465,
+        78,
+        243,
+        576,
+        789,
+        5,
+        7,
+        23,
+        58,
+        79,
+        25432,
+    ]
+
+
+# edge case
+def test_concat3() -> None:
+    assert concat([], []) == []
