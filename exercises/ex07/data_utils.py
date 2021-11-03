fd51d0be45dc5fe4298dccd1bfcf301bf279093b
mmoghul <mmoghul@live.unc.edu>
2021-11-02 23:13:47 -0400

    ex07

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -42,3 +42,35 @@ def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
         result[column] = column_values(row_table, column)
 
     return result
+
+def head(tabulate: dict[str, list[str]], N: [int]) -> dict[str, list[str]]: 
+    """Produce a new column-based table with only the first `N` rows of data for each column. """
+    result: dict[str, list[str]] = {}
+    N: [int] = #first row of table 
+    while N > 0: 
+        store: list[str] = []
+        N+=1 
+        result.append(store)
+
+    return result  
+
+def concat(table1: dict[str, list[str]], table2: dict[str, list[str]]) -> dict[str, list[str]]
+    """Produce a new column-based table with two column-based tables combined. """
+    result: dict[str, list[str]] = {}
+     while table1 > 0: 
+         N+=1 
+        result.append(store)
+    while table2 > 0: 
+
+
+    Loop through each of the columns _in the first parameter of the function_
+    1. Assign to the column key of the result dictionary the list of values stored in the first parameter at the same column
+3. Loop through each of the columns _in the second parameter of the function_
+    1. If the current column key is already in the result dictionary, add on the list of values stored in the second parameter at the same column 
+    2. Otherwise, just assign to the column key of the result dictionary the list of values stored in the second parameter at the same column
+
+def select(subset1: dict[str, list[str]], subset2: list[str]) -> dict[str, list[str]]:
+   """Produce a new column-based table with only a specific subset of the original columns. """
+   result: dict[str, list[str]] = {} 
+   Loop through each of the columns _in the second parameter of the function_
+    1. Assign to the column key of the result dictionary the list of values stored in the input dictionary at the same column 
